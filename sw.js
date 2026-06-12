951493a1546e19168822058e3aa572fcf04a4e0c
cuzzilladavide98-lgtm <cuzzilladavide98@gmail.com>
2026-06-12 09:58:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sentiero-v3';
+const CACHE = 'sentiero-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
 
 self.addEventListener('install', e => {
