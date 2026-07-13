42c450245fc7b76841eee094c66a475a0ab8982c
cuzzilladavide98-lgtm <cuzzilladavide98@gmail.com>
2026-07-13 12:12:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sentiero-v60s114';
+const CACHE = 'sentiero-v60s115';
 const ASSETS = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png', './icon-180.png',
   'splash-1290x2796.png', 'splash-1179x2556.png', 'splash-1170x2532.png',
